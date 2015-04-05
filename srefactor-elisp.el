5737c8fb36849c94ea3f4cdb3dcf2f0eb5676e28
Tu Do <tuhdo1710@gmail.com>
2015-04-05 22:19:17 +0700

    Finally transforming without moving point
    Since transforming sexp deletes its text, save-excursion can't help. We
    must store offset of current point with the start of sexp where this
    transfromation begins. After the transforming finishes, jumps back to
    the offset.
    
    Comments are also updated.

--- srefactor-elisp.el
+++ srefactor-elisp.el
@@ -20,6 +20,23 @@
 ;;
 ;;; Commentary:
 ;;
+;; Semantic is a package that provides a framework for writing
+;; parsers. Parsing is a process of analyzing source code based on
+;; programming language syntax. This package relies on Semantic for
+;; analyzing source code and uses its results to perform smart code
+;; refactoring that based on code structure of the analyzed language,
+;; instead of plain text structure.
+;;
+;; This package provides the following features for Emacs Lisp:
+;;
+;; - `srefactor-one-line': Transform all sub-sexpressions current sexpression at
+;; point into one line separated each one by a space.
+;;
+;; - `srefactor-multi-line': Transform all sub-sexpressions current sexpression
+;; - at point into multiple lines separated. If the head symbol belongs to the
+;; - list `srefactor-symbol-to-skip', then the first N next symbol/sexpressions
+;; - (where N is the nummber associated with the head symbol as stated in the
+;; - list) are skipped before a newline is inserted.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -44,6 +61,8 @@
                                       ("while" . 1)
                                       ("defun" . 2)
                                       ("lambda" . 2)
+                                      ("defvar" . 2)
+                                      ("defcustom" . 3)
                                       ("if" . 1)
                                       ("when" . 1)
                                       ("unless" . 1)
@@ -54,70 +73,84 @@
   sexp to be skipped before inserting the first newline. ")
 
 (defun srefactor-one-line ()
+  "Transform all sub-sexpressions current sexpression at point
+into one line separated each one by a space."
   (interactive)
   (srefactor-one-or-multi-lines 'one-line))
 
 (defun srefactor-multi-line ()
+  "Transform all sub-sexpressions current sexpression at point
+into multiple lines separated. If the head symbol belongs to the
+list `srefactor-symbol-to-skip', then the first N next
+symbol/sexpressions (where N is the nummber associated with the
+head symbol as stated in the list) are skipped before a newline
+is inserted."
   (interactive)
   (srefactor-one-or-multi-lines 'multi-line))
 
 (defun srefactor-one-or-multi-lines (format-type)
-  (save-excursion
-    (unless (looking-at "(")
-      (backward-up-list))
-    (let* ((tag-start (point))
-           (tag-end (save-excursion
-                      (forward-sexp)
+  "Turn the current sexpression into one line/multi-line depends
+on the value of FORMAT-TYPE. If FORMAT-TYPE is 'one-line,
+transforms all sub-sexpressions of the same level into one
+line. If FORMAT-TYPE is 'multi-line, transforms all
+sub-sexpressions of the same level into multiple lines."
+  (let* ((orig-point (point))
+         (tag-start (progn
+                      (unless (looking-at "(")
+                        (backward-up-list))
                       (point)))
-           (lexemes (semantic-emacs-lisp-lexer tag-start tag-end 1))
-           (first-symbol (cadr lexemes))
-           (first-symbol-name (buffer-substring-no-properties
-                               (semantic-lex-token-start first-symbol)
-                               (semantic-lex-token-end first-symbol)))
-           (tmp-buf (generate-new-buffer "let-buf"))
-           start end token-str ignore-pair token ignore-num)
-      (setq start (point))
-      (while lexemes
-        (setq token (pop lexemes))
-        (setq token-str (if token
-                            (buffer-substring-no-properties
-                             (semantic-lex-token-start token)
-                             (semantic-lex-token-end token))
-                          ""))
-        (let* ((token-type (car token))
-               (next-token (car lexemes))
-               (next-token-type (car next-token)))
-          (with-current-buffer tmp-buf
-            (insert token-str)
-            (cond
-             ((or (and (eq token-type 'punctuation)
-                       (equal token-str "'"))
-                  (eq token-type 'open-paren)
-                  (eq token-type 'close-paren)
-                  (eq next-token-type 'close-paren))
-              "")
-             ((eq format-type 'one-line)
-              (insert " "))
-             ((eq format-type 'multi-line)
-              (if (and (eq token-type 'symbol)
-                       (string-match ":.*" token-str))
-                  (insert " ")
-                (insert "\n")))))))
-      (setq end (point))
-      (kill-region start tag-end)
-      (goto-char start)
-      (insert (with-current-buffer tmp-buf
-                (buffer-substring-no-properties (point-min)
-                                                (point-max))))
-      (when (eq format-type 'multi-line)
-        (when (setq ignore-pair (assoc first-symbol-name srefactor-symbol-to-skip))
-          (save-excursion
-            (setq ignore-num (cdr ignore-pair))
-            (goto-char tag-start)
-            (while (> ignore-num 0)
-              (forward-line 1)
-              (delete-indentation)
-              (setq ignore-num (1- ignore-num))))))
-      (indent-region tag-start (point)))))
+         (tag-end (save-excursion
+                    (forward-sexp)
+                    (point)))
+         (lexemes (semantic-emacs-lisp-lexer tag-start tag-end 1))
+         (first-symbol (cadr lexemes))
+         (first-symbol-name (buffer-substring-no-properties
+                             (semantic-lex-token-start first-symbol)
+                             (semantic-lex-token-end first-symbol)))
+         (tmp-buf (generate-new-buffer (make-temp-name "")))
+         token-str ignore-pair token ignore-num)
+    (while lexemes
+      (setq token (pop lexemes))
+      (setq token-str (if token
+                          (buffer-substring-no-properties
+                           (semantic-lex-token-start token)
+                           (semantic-lex-token-end token))
+                        ""))
+      (let* ((token-type (car token))
+             (next-token (car lexemes))
+             (next-token-type (car next-token)))
+        (with-current-buffer tmp-buf
+          (insert token-str)
+          (cond
+           ((or (and (eq token-type 'punctuation)
+                     (equal token-str "'"))
+                (eq token-type 'open-paren)
+                (eq token-type 'close-paren)
+                (eq next-token-type 'close-paren))
+            "")
+           ((eq format-type 'one-line)
+            (insert " "))
+           ((eq format-type 'multi-line)
+            (if (and (eq token-type 'symbol)
+                     (string-match ":.*" token-str))
+                (insert " ")
+              (insert "\n")))))))
+    (kill-region tag-start tag-end)
+    (goto-char tag-start)
+    (insert (with-current-buffer tmp-buf
+              (buffer-substring-no-properties (point-min)
+                                              (point-max))))
+    (setq tag-end (point))
+    (when (eq format-type 'multi-line)
+      (when (setq ignore-pair (assoc first-symbol-name srefactor-symbol-to-skip))
+        (save-excursion
+          (setq ignore-num (cdr ignore-pair))
+          (goto-char tag-start)
+          (while (> ignore-num 0)
+            (forward-line 1)
+            (delete-indentation)
+            (setq ignore-num (1- ignore-num))))))
+    (indent-region tag-start tag-end)
+    (goto-char (+ tag-start (- orig-point tag-start)))))
 
 (provide 'srefactor-elisp)
