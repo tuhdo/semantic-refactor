b203e0e47e754067bed53cbf7f536ddc9e85afc2
Tu Do <tuhdo1710@gmail.com>
2016-11-08 12:56:59 +0700

    Refactor
    - Separate srecode code into a function
    - Don't insert a newline before a generated function prototype.
    - Only insert 1 newline before generated function defintion.

--- srefactor.el
+++ srefactor.el
@@ -765,41 +765,50 @@ BUFFER is the destination buffer from file user selects from contextual menu."
 ;;
 ;; FUNCTION
 ;;
+(defun srefactor--insert-with-srecode (func-tag)
+  "Insert a tag using srecode"
+  (let* ((copy (semantic-tag-copy func-tag))
+         ;; (parent (semantic-tag-calculate-parent func-tag))
+         ;; TODO - below srefactor fcn should be a part of semantic or srecode.
+         (parentstring1 (srefactor--tag-parents-string func-tag))
+         (parentstring (substring parentstring1 0 (- (length parentstring1) 2)))
+         (endofinsert nil))
+    ;; Copied this line from original
+    (semantic-tag-put-attribute func-tag :typemodifiers nil)
+    (semantic-tag-put-attribute func-tag :parent parentstring)
+    ;; Insert the tag
+    (require 'srecode/semantic)
+    ;; TODO - does it need any special dictionary entries?
+    (setq endofinsert
+          (srecode-semantic-insert-tag
+           func-tag
+           nil ;; Style
+           (lambda (localtag)
+             (srefactor--insert-initial-content-based-on-return-type
+              (if (or (srefactor--tag-function-constructor copy)
+                      (srefactor--tag-function-destructor copy))
+                  ""
+                (semantic-tag-type copy)))
+             ) ;; Callbck for function body.
+           ;; Dictionary entries go here.
+           ))
+    (goto-char endofinsert)
+    (insert "\n\n")))
+
+(defmacro srefactor--is-proto (type)
+  `(eq ,type 'gen-func-proto))
+
 (defun srefactor--insert-function (func-tag type)
-  "Insert function implementations for FUNC-TAG at point, a tag that is a function."
+  "Insert function implementations for FUNC-TAG at point, a tag that is a function.
+`type' is the operation to be done, not the type of the tag."
   (if srefactor-use-srecode-p
       ;; Try using SRecode as the mechanism for inserting a tag.
-      (let* ((copy (semantic-tag-copy func-tag))
-             ;; (parent (semantic-tag-calculate-parent func-tag))
-             ;; TODO - below srefactor fcn should be a part of semantic or srecode.
-             (parentstring1 (srefactor--tag-parents-string func-tag))
-             (parentstring (substring parentstring1 0 (- (length parentstring1) 2)))
-             (endofinsert nil))
-        ;; Copied this line from original
-        (semantic-tag-put-attribute func-tag :typemodifiers nil)
-        (semantic-tag-put-attribute func-tag :parent parentstring)
-        ;; Insert the tag
-        (require 'srecode/semantic)
-        ;; TODO - does it need any special dictionary entries?
-        (setq endofinsert
-              (srecode-semantic-insert-tag
-               func-tag
-               nil ;; Style
-               (lambda (localtag)
-                 (srefactor--insert-initial-content-based-on-return-type
-                  (if (or (srefactor--tag-function-constructor copy)
-                          (srefactor--tag-function-destructor copy))
-                      ""
-                    (semantic-tag-type copy)))
-                 ) ;; Callbck for function body.
-               ;; Dictionary entries go here.
-               ))
-        (goto-char endofinsert)
-        (insert "\n\n"))
+      (srefactor--insert-with-srecode func-tag)
     ;; official routine
     ;; add 2 newlines before insert the function
-    (newline-and-indent)
-    (newline-and-indent)
+    ;; (newline-and-indent)
+    (unless (srefactor--is-proto type)
+      (newline-and-indent))
     (let ((func-tag-name (srefactor--tag-name func-tag))
           (parent (srefactor--calculate-parent-tag func-tag)))
       (when (srefactor--tag-function-modifiers func-tag)
@@ -809,11 +818,12 @@ BUFFER is the destination buffer from file user selects from contextual menu."
                    parent)
           (insert (srefactor--tag-templates-declaration-string parent)))
         (insert (srefactor--tag-function-string func-tag))
-        (when (eq type 'gen-func-proto)
+        (when (srefactor--is-proto type)
           (insert ";\n")))
       (unless (eq major-mode 'c-mode)
         (search-forward-regexp (regexp-quote func-tag-name) (line-end-position) t)
         (search-backward-regexp (regexp-quote func-tag-name) (line-beginning-position) t)
+
         (when (srefactor--tag-function-destructor func-tag)
           (forward-char -1))
 
